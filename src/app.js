e49a653a3983be68990598d6e9cc649e003834de
Maxence Chevee <max.rothen@hotmail.fr>
2023-11-30 11:45:21 +0100

    finish Express02

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
 
@@ -10,5 +13,7 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", userControllers.postUser)
 
 module.exports = app;
