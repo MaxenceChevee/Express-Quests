113d420cf12b94d5d5af93b66a354bd718eafb2f
Maxence Chevee <max.rothen@hotmail.fr>
2023-12-01 14:40:28 +0100

    correction express_03.1

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -122,17 +122,7 @@ describe("PUT /api/movies/:id", () => {
     expect(movieInDatabase).toHaveProperty("title");
     expect(movieInDatabase.title).toStrictEqual(updatedMovie.title);
 
-    expect(movieInDatabase).toHaveProperty("director");
-    expect(movieInDatabase.director).toStrictEqual(updatedMovie.director);
 
-    expect(movieInDatabase).toHaveProperty("year");
-    expect(movieInDatabase.year).toStrictEqual(updatedMovie.year);
-
-    expect(movieInDatabase).toHaveProperty("color");
-    expect(movieInDatabase.color).toStrictEqual(updatedMovie.color);
-
-    expect(movieInDatabase).toHaveProperty("duration");
-    expect(movieInDatabase.duration).toStrictEqual(updatedMovie.duration);
   });
   it("should return an error", async () => {
     const movieWithMissingProps = { title: "Harry Potter" };
@@ -146,10 +136,7 @@ describe("PUT /api/movies/:id", () => {
   it("should return no movie", async () => {
     const newMovie = {
       title: "Avatar",
-      director: "James Cameron",
-      year: "2009",
-      color: "1",
-      duration: 162,
+
     };
 
     const response = await request(app).put("/api/movies/0").send(newMovie);
