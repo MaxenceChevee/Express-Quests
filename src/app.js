a05d8cb55a39d1a3b1c1856ce3eeb6df3bf73d38
Maxence Chevee <max.rothen@hotmail.fr>
2023-12-04 14:20:39 +0100

    finish express04

--- src/app.js
+++ src/app.js
@@ -4,20 +4,24 @@ const app = express();
 
 app.use(express.json());
 
-
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
 const validateMovie = require("./middlewares/validateMovie");
 const validateUser = require("./middlewares/validateUser");
 
-
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
+
 app.post("/api/movies", validateMovie, movieControllers.postMovie);
 app.post("/api/users", validateUser, userControllers.postUser);
+
 app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
 app.put("/api/users/:id", validateUser, userControllers.updateUser);
+
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
+app.delete("/api/users/:id", userControllers.deleteUser);
 
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -1,4 +1,31 @@
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
 const database = require("../../database");
+
 const getMovies = (req, res) => {
   database
     .query("select * from movies")
@@ -15,7 +42,7 @@ const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from movies where id = ?", [id])
+    .query("Select * from movies where id = ?", [id])
     .then(([movies]) => {
       if (movies[0] != null) {
         res.json(movies[0]);
@@ -28,9 +55,9 @@ const getMovieById = (req, res) => {
       res.sendStatus(500);
     });
 };
+
 const postMovie = (req, res) => {
   const { title, director, year, color, duration } = req.body;
-
   database
     .query(
       "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
@@ -42,7 +69,7 @@ const postMovie = (req, res) => {
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
-    })
+    });
 };
 const updateMovie = (req, res) => {
   const id = parseInt(req.params.id);
@@ -65,9 +92,29 @@ const updateMovie = (req, res) => {
       res.sendStatus(500);
     });
 };
+
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
   postMovie,
   updateMovie,
+  deleteMovie,
 };
